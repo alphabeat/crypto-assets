ae97d51a5feb03d645a441798f4de525994279cc
Julien Kilo <julienkilo@gmail.com>
2020-06-12 19:10:48 +0200

    add action to create a dashboard with pre-filled data

--- components/DashboardForm/index.js
+++ components/DashboardForm/index.js
@@ -2,20 +2,58 @@ import { useRouter } from 'next/router'
 
 const API_URL = `http://localhost:3000/api/dashboard`
 
+const DEFAULT_TICKERS = [
+  { platform: 'bittrex', coin: 'BTC', market: 'EUR' },
+  { platform: 'bittrex', coin: 'BTC', market: 'USD' },
+  { platform: 'kraken', coin: 'ETH', market: 'EUR' },
+  { platform: 'kraken', coin: 'ETH', market: 'USD' },
+]
+
+const DEFAULT_ASSETS = [
+  { platform: 'bittrex', coin: 'BTC', balance: 1.00503293, initialValue: 1.00503293 },
+  { platform: 'kraken', coin: 'BTC', balance: 0.21402864, initialValue: 0.21402864 },
+  { platform: 'kraken', coin: 'ETH', balance: 32.6, initialValue: 0.00823043 },
+  { platform: 'kraken', coin: 'XRP', balance: 4545, initialValue: 0.00005273 },
+]
+
+const getRecordId = (record) => record.ref['@ref'].id
+
 function DashboardForm(props) {
   const { show, handleClose } = props
   const router = useRouter()
   const activeClass = show ? 'is-active' : ''
 
-  async function createEmptyDashboard() {
+  const createRecord = async (recordType, dashboardRef, data) => {
+    return fetch(`${API_URL}/${dashboardRef}/${recordType}`, {
+      method: 'POST',
+      body: JSON.stringify({
+        ...data,
+        dashboard: dashboardRef,
+      }),
+    })
+  }
+
+  async function createTickersAndAssets(dashboardRef) {
+    return Promise.all([
+      ...DEFAULT_TICKERS.map((ticker) => createRecord('tickers', dashboardRef, ticker)),
+      ...DEFAULT_ASSETS.map((asset) => createRecord('assets', dashboardRef, asset)),
+    ])
+  }
+
+  async function createDashboard(isFilled = false) {
     try {
       const response = await fetch(API_URL, {
         method: 'POST'
       })
       const result = await response.json()
 
-      console.log(result)
       const { dashboardId } = result.data
+
+      if (isFilled) {
+        const dashboardRef = getRecordId(result)
+
+        await createTickersAndAssets(dashboardRef)
+      }
 
       router.push(`/dashboard/${dashboardId}`)
     }
@@ -44,12 +82,12 @@ function DashboardForm(props) {
           <section className="card-content">
             <div className="columns has-text-centered">
               <div className="column">
-                <button className="button" onClick={() => createEmptyDashboard()}>
+                <button className="button" onClick={() => createDashboard(false)}>
                   Empty dashboard
                 </button>
               </div>
               <div className="column">
-                <button className="button is-primary">
+                <button className="button is-primary" onClick={() => createDashboard(true)}>
                   Filled dashboard
                 </button>
               </div>
