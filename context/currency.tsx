a69b1a6c502d819d240f94f4f5e611c9c2f67d3a
Julien Kilo <julienkilo@gmail.com>
2021-05-15 11:20:24 +0200

    Add a context to manage currency

---
+++ context/currency.tsx
@@ -0,0 +1,22 @@
+import React, { useContext, useState } from 'react'
+
+type Currency = 'EUR' | 'USD'
+
+type CurrencyContextType = {
+  currency: Currency
+  setCurrency: (currency: Currency) => void
+}
+
+const CurrencyContext = React.createContext<CurrencyContextType>({} as CurrencyContextType)
+
+const CurrencyProvider: React.FC = (props) => {
+  const [currency, setCurrency] = useState<Currency>('EUR')
+
+  return (
+    <CurrencyContext.Provider value={{ currency, setCurrency }} {...props} />
+  )
+}
+
+const useCurrency = () => useContext(CurrencyContext)
+
+export { CurrencyProvider, useCurrency };
