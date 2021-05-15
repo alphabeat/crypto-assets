8b775df0820e5e574e8cab661e82066e30209a7c
Julien Kilo <julienkilo@gmail.com>
2021-05-15 10:32:10 +0200

    Update Sass uploads to work with Next v10.2

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import '../../styles/styles.sass'
 
 type LayoutProps = {
   hasHero: boolean
--- next.config.js
+++ next.config.js
@@ -1,6 +1,3 @@
-const withSass = require('@zeit/next-sass')
-
 module.exports = {
   target: 'serverless',
-  ...withSass()
 }
